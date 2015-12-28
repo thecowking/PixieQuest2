2f87934462da755dbad90061d895b9785325ddcd
Dominic Kua <ravisherkua+github@gmail.com>
2015-12-28 09:18:29 +0000

    updated to add basic persistence and colour change to the screen

--- src/pixie_quest_2.py
+++ src/pixie_quest_2.py
@@ -1,7 +1,18 @@
 import pygame
+import time
+
+def update_screen(screen):
+	for x in range(255):
+		screen.fill((x,x,x))
+		print x
+		pygame.display.update()
+		time.sleep(.1)
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1024, 768), FULLSCREEN)
-    screen.fill('white')
-    screen.blit()
+    #clock = pygame.time.clock()
+    screen = pygame.display.set_mode((1024, 768))
+    update_screen(screen)
+
+main()
